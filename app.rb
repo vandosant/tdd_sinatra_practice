29ed1f82add18c212246e53bc86432c98a21f51a
vandosant <sjskender@gmail.com>
2014-03-29 14:54:09 -0600

    User is able to create a task

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Application
+  TASKS = []
+
   get '/' do
-    "welcome"
+    erb :index
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/' do
+    TASKS << params[:new_task]
+    redirect '/'
   end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,9 +4,15 @@ require_relative '../app'
 Capybara.app = App
 
 feature "task management" do
-  scenario "user sees a welcome message" do
+  scenario "user is able to create a task" do
     visit '/'
 
     expect(page).to have_content("welcome")
+
+    click_link "Add a Task"
+    fill_in "new_task", with: "buy milk"
+    click_on "Create Task"
+
+    expect(page).to have_content("buy milk")
   end
 end
